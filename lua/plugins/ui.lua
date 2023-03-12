691068250cc2013d8ed1bca2726798aff450442d
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2023-03-13 01:39:41 +0530

    clear lua line

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,6 +1,36 @@
 return {
   {
     "rcarriga/nvim-notify",
+    opts = { render = "compact", top_down = false },
+  },
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function()
+      local icons = require("lazyvim.config").icons
+      return {
+        sections = {
+          lualine_c = {
+            {
+              "diagnostics",
+              symbols = {
+                error = icons.diagnostics.Error,
+                warn = icons.diagnostics.Warn,
+                info = icons.diagnostics.Info,
+                hint = icons.diagnostics.Hint,
+              },
+            },
+            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+            { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+          },
+        },
+      }
+    end,
+  },
+
+  -- lsp symbol navigation
+  {
+    "SmiteshP/nvim-navic",
     enabled = false,
   },
 }
