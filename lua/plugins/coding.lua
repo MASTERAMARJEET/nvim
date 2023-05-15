5c5af2d7027a6de09eac3d9b4535cbef84c319ab
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2023-05-15 13:23:18 +0530

    add custom snippets

---
+++ lua/plugins/coding.lua
@@ -0,0 +1,17 @@
+return {
+
+  -- snippets
+  {
+    "L3MON4D3/LuaSnip",
+    build = (not jit.os:find("Windows"))
+        and "echo -e 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
+      or nil,
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
+      end,
+    },
+  },
+}
