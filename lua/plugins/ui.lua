bf0ac609de608e709bef2151aee14740cd67ff21
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2025-11-30 10:34:52 +0530

    feat: :sparkles: change the position of file explorer from left to right

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -13,6 +13,28 @@ function custom_filename:update_status()
 end
 
 return {
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      explorer = {
+        -- your explorer configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      picker = {
+        sources = {
+          explorer = {
+            layout = {
+              layout = {
+                position = "right",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
   -- toggleterm
   {
     "akinsho/toggleterm.nvim",
